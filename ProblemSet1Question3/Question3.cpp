9347c980e031d687cb398ce3d9f110c443b5e322
Christian S <Saregadon@gmail.com>
2021-02-22 15:19:17 -0600

    Set up bool for Part 3

--- ProblemSet1Question3/Question3.cpp
+++ ProblemSet1Question3/Question3.cpp
@@ -40,17 +40,17 @@ bool functionone(int &a, int &b, int &c, int &d, int &e, int &f, int &nva)
 
 bool functiontwo(int& a, int& b, int& c, int &d, int& e, int& f, int &g, int &h, int &i, int &j, int& nva)
 {
-    for(g = 1; g < 51; g++, nva++)
+    for(g = 1; g < 51; g++, nva++) //time complexity = n
     {
         if(a + d == pow(f - g , 2) - 1 && pow(g - c, 2) == f*(pow(c,2)) + 1) //problem 6 and 9
         {
-            for(j = 1; j < 51; j++, nva++)
+            for(j = 1; j < 51; j++, nva++) //time complexity = n^2
             {
                 if(4*j == pow(g, 2) + 39) //problem 7
                 {
-                    for(h = 1; h < 51; h++, nva++)
+                    for(h = 1; h < 51; h++, nva++) //time complexity = n^3
                     {
-                        for(i = 1; i < 51; i++, nva++) //time complexity = n^
+                        for(i = 1; i < 51; i++, nva++) //time complexity = n^4
                         {
                             if((h*j + e*12) == pow(g + i, 2) && pow(i-g, 9) == pow(f-h, 3)) //problem 5 and 8
                             {
@@ -66,19 +66,22 @@ bool functiontwo(int& a, int& b, int& c, int &d, int& e, int& f, int &g, int &h,
     return false;
 }
 
-bool functionthree(int& A, int& B, int& C, int& D, int& E, int& F, int& Z, int& z)
+bool functionthree(int& a, int& b, int& c, int& d, int& e, int& f, int& g, int& h, int& i, int&j, int& k, int& l, int& m, int& n, int& o, int &nva)
 {
+
+
     return false;
 }
 
 int main() {
-    int a = 0, b = 0, c = 0, d = 0, e = 0, z = 0, f = 0;
-    int g = 0, h = 0, i = 0, j = 0;
+    int a = 0, b = 0, c = 0, d = 0, e = 0, z = 0, f = 0; //Problem A
+    int g = 0, h = 0, i = 0, j = 0; //Problem B
+    int k = 0, l = 0, m = 0, n = 0, o = 0; //Problem C
     int nva = 0; //number variable assignments
 
-    bool works = functionone(a, b, c, d, e, f, nva);
+    bool Problem2PartA = functionone(a, b, c, d, e, f, nva);
 
-    if (works == true)
+    if (Problem2PartA == true)
     {
         cout << "Correct, after " << nva << " assignments!" << endl;
         cout << " a =" << a << "| b =" << b << "| c =" << c << "| d =" << d << "| e =" << e << "| f =" << f << "| nva =" << nva << endl;
@@ -87,8 +90,8 @@ int main() {
         cout << "Rip failed Question 1, after " << nva << "assignments. Be better." << endl;
     //cout << endl << "Q.E.D" << endl;
 
-    bool contworks = functiontwo(a, b, c, d, e, f, g, h, i, j, nva);
-    if (contworks == true)
+    bool Problem2PartB = functiontwo(a, b, c, d, e, f, g, h, i, j, nva);
+    if (Problem2PartB == true)
     {
         cout << "Correct once again, after " << nva << " assignments!" << endl;
         cout << " a =" << a << "| b =" << b << "| c =" << c << "| d =" << d << "| e =" << e << "| f =" << f << "| g = " << g << "| h = " << h << "| i = " << i << "| j = " << j << "| nva =" << nva << endl;
@@ -96,6 +99,13 @@ int main() {
     else 
         cout << "Rip failed Question 2, after " << nva << " assignments. Be better." << endl;
     //cout << endl << "Q.E.D" << endl;
+
+    bool Problem2PartC = functionthree(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, nva)
+    if(Problem2PartC == true)
+    {
+        cout << "Correct once again, after " << nva << " assignments!" << endl;
+        cout << " a =" << a << "| b =" << b << "| c =" << c << "| d =" << d << "| e =" << e << "| f =" << f << "| g = " << g << "| h = " << h << "| i = " << i << "| j = " << j << "| nva =" << nva << endl;
+    }
 
     return 0;
 }
